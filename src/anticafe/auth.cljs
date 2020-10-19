d7073a4c60dbc7ad637a92922eb1623b915f54d6
em-ak <emak@emak.re>
2020-10-19 05:34:57 +0200

    Refactor: Organize anticafe.auth
    no code changes

--- src/anticafe/auth.cljs
+++ src/anticafe/auth.cljs
@@ -4,6 +4,36 @@
     [oops.core :refer [oget ocall! ocall]]
     [re-frame.core :as rf]))
 
+;; Effect handlers
+
+(rf/reg-fx
+  :auth/listen-ethereum
+  (fn [{:keys [trigger handler]}]
+    (-> js/window
+        (oget "ethereum")
+        (ocall! "on" trigger (comp handler js->clj)))))
+
+(rf/reg-fx
+  :auth/request-ethereum
+  (fn [{:keys [method on-success on-error]}]
+    (-> js/window
+        (oget "ethereum")
+        (ocall "request" (clj->js {:method method}))
+        (.then (comp on-success js->clj))
+        (.catch on-error))))
+
+;; Coeffect handlers
+
+(rf/reg-cofx
+  :auth/ethereum
+  (fn [cofx _]
+    (let [metamask? (oget js/window "?ethereum.?isMetaMask")
+          chain-id  (oget js/window "?ethereum.?chainId")]
+      (assoc cofx :ethereum {:metamask? metamask?
+                             :chain-id chain-id}))))
+
+;; Ethereum network
+
 (def networks
   {"0x1"  :mainnet
    "0x3"  :ropsten
@@ -16,13 +46,13 @@
   (fn [db _]
     (get-in db [:auth :metamask-installed?])))
 
-(defn- set-network [effects ethereum]
-  (assoc-in effects [:db :auth :network] (get networks (:chain-id ethereum))))
-
 (rf/reg-sub
   :auth/network
   (fn [db _]
     (get-in db [:auth :network])))
+
+(defn- set-network [effects ethereum]
+  (assoc-in effects [:db :auth :network] (get networks (:chain-id ethereum))))
 
 (rf/reg-sub
   :auth/valid-network?
@@ -30,25 +60,10 @@
   (fn [network]
     (= network :rinkeby)))
 
-(rf/reg-sub
-  :auth/account
-  (fn [db _]
-    (get-in db [:auth :accounts 0])))
-
-(rf/reg-cofx
-  :auth/ethereum
-  (fn [cofx _]
-    (let [metamask? (oget js/window "?ethereum.?isMetaMask")
-          chain-id  (oget js/window "?ethereum.?chainId")]
-      (assoc cofx :ethereum {:metamask? metamask?
-                             :chain-id chain-id}))))
-
-(rf/reg-fx
-  :auth/listen-ethereum
-  (fn [{:keys [trigger handler]}]
-    (-> js/window
-        (oget "ethereum")
-        (ocall! "on" trigger (comp handler js->clj)))))
+(defn- listening? [db trigger]
+  (-> db
+      (get-in [:auth :listeners])
+      (contains? trigger)))
 
 (rf/reg-event-fx
   :auth/change-chain
@@ -57,11 +72,6 @@
     (-> {:db db}
         (update-in [:db :auth :listeners] #(-> % (conj "chainChanged") set))
         (set-network ethereum))))
-
-(defn- listening? [db trigger]
-  (-> db
-      (get-in [:auth :listeners])
-      (contains? trigger)))
 
 (rf/reg-event-fx
   :auth/init
@@ -79,16 +89,12 @@
                         {:trigger "chainChanged"
                          :handler #(>evt [:auth/change-chain])}]))))
 
-;; Connect to user account
+;; User account
 
-(rf/reg-fx
-  :auth/request-ethereum
-  (fn [{:keys [method on-success on-error]}]
-    (-> js/window
-        (oget "ethereum")
-        (ocall "request" (clj->js {:method method}))
-        (.then (comp on-success js->clj))
-        (.catch on-error))))
+(rf/reg-sub
+  :auth/account
+  (fn [db _]
+    (get-in db [:auth :accounts 0])))
 
 (rf/reg-event-db
   :auth/change-account
